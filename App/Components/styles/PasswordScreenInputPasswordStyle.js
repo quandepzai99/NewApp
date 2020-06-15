95e66bf3f525113d5f33d17bee19d838b86609b3
Veiler <linhnv110196@gmail.com>
2020-06-15 23:08:50 +0700

    add pws scr input password & styles

---
+++ App/Components/styles/PasswordScreenInputPasswordStyle.js
@@ -0,0 +1,77 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: "center",
+        backgroundColor: "#fff",
+        marginHorizontal: 16,
+        borderRadius: 24,
+        marginTop: -40,
+        shadowColor: "#000",
+        shadowOpacity: 0.23,
+        shadowRadius: 2.62,
+        elevation: 4
+    },
+    text1: {
+        marginTop: 32,
+        fontSize: 17,
+        fontWeight: "bold",
+        fontStyle: "normal",
+        lineHeight: 24,
+        letterSpacing: 0,
+        textAlign: "center"
+    },
+    box: {
+        flexDirection: "row",
+        marginTop: 35
+    },
+    btSignOut: {
+        marginRight: 16,
+        top: -5,
+        flexDirection: "row",
+        paddingTop: 15,
+        left: 50
+    },
+    text2: {
+        marginLeft: 10,
+        right: 30,
+        fontSize: 14,
+        fontWeight: "normal",
+        fontStyle: "normal",
+        lineHeight: 24,
+        letterSpacing: 0,
+
+        color: colors.deepSkyBlue
+    },
+    text3: {
+        marginRight: 10,
+        fontSize: 15,
+        fontWeight: "normal",
+        fontStyle: "normal",
+        lineHeight: 24,
+        letterSpacing: 0,
+        textAlign: "left",
+        color: colors.red
+    },
+    box2: {
+        borderBottomLeftRadius: 24,
+        borderBottomRightRadius: 24,
+        marginTop: 24,
+        width: "100%",
+        alignItems: "center",
+        borderTopWidth: 0.5
+    },
+    text4: {
+        marginTop: 14,
+        marginBottom: 14,
+        fontSize: 14,
+        fontWeight: "normal",
+        fontStyle: "normal",
+        lineHeight: 24,
+        letterSpacing: 0,
+        textAlign: "left",
+        color: colors.blueGrey
+    }
+});
+
+export default styles;
