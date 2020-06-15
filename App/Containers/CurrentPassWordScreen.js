c15d1e86a152b0d8beb05f7dc813f5fd0174f3a9
quandepzai <43404519+quandepzai@users.noreply.github.com>
2020-06-15 18:16:30 +0700

    style pin-input

--- App/Containers/CurrentPassWordScreen.js
+++ App/Containers/CurrentPassWordScreen.js
@@ -3,7 +3,7 @@ import { View, Text, StatusBar, TouchableOpacity } from "react-native";
 import HeaderCurrentPassword from "../Components/HeaderCurrentPassword";
 import StyleHeaderCurrentPassword from "../Components/styles/StyleHeaderCurrentPassword";
 import AntDesign from "react-native-vector-icons/AntDesign";
-// import PinInputCurrent from '../Components/PinInputCurrent';
+import PinInputCurrent from '../Components/PinInputCurrent';
 // import { navigationRef } from "../RootNavigation";
 
 // function navigate(name) {
@@ -15,32 +15,32 @@ export default function CurrentPassWordScreen() {
     <View>
       <StatusBar barStyle={"light-content"} />
       <HeaderCurrentPassword />
-      {/*<View style={StyleHeaderCurrentPassword.viewBlock2}>*/}
-      {/*  <Text style={StyleHeaderCurrentPassword.textblock2box1}>Nhập mật khẩu hiện tại</Text>*/}
-      {/*  <View style={StyleHeaderCurrentPassword.viewBlock2box1}>*/}
-      {/*    /!*<PinInputCurrent />*!/*/}
-      {/*  </View>*/}
-      {/*</View>*/}
-      {/*<TouchableOpacity*/}
-      {/*  style={{*/}
-      {/*    flexDirection: "row",*/}
-      {/*    justifyContent: "center",*/}
-      {/*    marginTop: 26*/}
-      {/*  }}*/}
-      {/*  // onPress={() => navigate("PinCode")}*/}
-      {/*>*/}
-      {/*  <AntDesign*/}
-      {/*    name={"left"}*/}
-      {/*    size={15}*/}
-      {/*    color={"gray"}*/}
-      {/*    style={{*/}
-      {/*      paddingTop: 10,*/}
-      {/*      paddingBottom: 10,*/}
-      {/*      paddingLeft: 10*/}
-      {/*    }}*/}
-      {/*  />*/}
-      {/*  <Text style={StyleHeaderCurrentPassword.goBackButton}> Quay lại</Text>*/}
-      {/*</TouchableOpacity>*/}
+      <View style={StyleHeaderCurrentPassword.viewBlock2}>
+        <Text style={StyleHeaderCurrentPassword.textblock2box1}>Nhập mật khẩu hiện tại</Text>
+        <View style={StyleHeaderCurrentPassword.viewBlock2box1}>
+          <PinInputCurrent />
+        </View>
+      </View>
+      <TouchableOpacity
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          marginTop: 26
+        }}
+        // onPress={() => navigate("PinCode")}
+      >
+        <AntDesign
+          name={"left"}
+          size={20}
+          color={"gray"}
+          style={{
+            paddingTop: 10,
+            paddingBottom: 10,
+            paddingLeft: 10
+          }}
+        />
+        <Text style={StyleHeaderCurrentPassword.goBackButton}> Quay lại</Text>
+      </TouchableOpacity>
     </View>
   );
 }
