6186992792f13e12969da9ef1e3ff9a3a18bd061
murkrow92 <ducdt199@gmail.com>
2020-06-15 15:29:58 +0700

    add animation

---
+++ App/Data/EcomProductMockup.js
@@ -0,0 +1,77 @@
+import Image from "../Images/Sub/vali-808-hong.png";
+import LogoUrbox from "../Images/logo_final.png";
+
+export const ImageProduct = {
+  title:
+    "Tay Cầm Microsoft Xbox One S (Màu Trắng) Tay Cầm Microsoft Xbox One S (Màu Trắng)",
+  image: Image,
+  quantity: 1,
+  price: 4000000,
+  checked: true
+};
+
+export const NoImageProduct = {
+  title:
+    "Tay Cầm Microsoft Xbox One S (Màu Trắng) Tay Cầm Microsoft Xbox One S (Màu Trắng)",
+  image: null,
+  quantity: 1,
+  price: 5000000,
+  checked: false
+};
+
+export const Supplier = {
+  title: "UrBox",
+  logo: LogoUrbox,
+  note: "Lorem Ipsum",
+};
+
+export const EcomCartMockup = [
+  {
+    title:
+      "Tay Cầm Microsoft Xbox One S (Màu Trắng) Tay Cầm Microsoft Xbox One S (Màu Trắng)",
+    image: Image,
+    quantity: 1,
+    price: 4000000,
+    checked: true
+  },
+  {
+    title:
+      "Tay Cầm Microsoft Xbox One S (Màu Trắng) Tay Cầm Microsoft Xbox One S (Màu Trắng)",
+    image: Image,
+    quantity: 1,
+    price: 4000000,
+    checked: true
+  },
+  {
+    title:
+      "Tay Cầm Microsoft Xbox One S (Màu Trắng) Tay Cầm Microsoft Xbox One S (Màu Trắng)",
+    image: null,
+    quantity: 1,
+    price: 5000000,
+    checked: false
+  },
+  {
+    title:
+      "Tay Cầm Microsoft Xbox One S (Màu Trắng) Tay Cầm Microsoft Xbox One S (Màu Trắng)",
+    image: Image,
+    quantity: 1,
+    price: 4000000,
+    checked: true
+  },
+  {
+    title:
+      "Tay Cầm Microsoft Xbox One S (Màu Trắng) Tay Cầm Microsoft Xbox One S (Màu Trắng)",
+    image: Image,
+    quantity: 1,
+    price: 4000000,
+    checked: true
+  },
+  {
+    title:
+      "Tay Cầm Microsoft Xbox One S (Màu Trắng) Tay Cầm Microsoft Xbox One S (Màu Trắng)",
+    image: null,
+    quantity: 1,
+    price: 5000000,
+    checked: false
+  }
+];
