e923a33e2541d013763e9db73ca78521f2fc7218
Veiler <linhnv110196@gmail.com>
2020-06-16 16:21:54 +0700

    Convert LoginScreen sang hook

--- App/Containers/LoginScreen.js
+++ App/Containers/LoginScreen.js
@@ -1,46 +1,29 @@
-import React, { Component } from "react";
+import React, { Component, useContext } from "react";
 import { View, StatusBar } from "react-native";
 import LoginScreenInputPhoneNumber from "../Components/LoginScreenInputPhoneNumber";
 import LoginScreenHeader from "../Components/LoginScreenHeader";
 import GlobalChatPopUpIcon from "../Components/GlobalChatPopUpIcon";
+import { LanguageContext } from "../Providers/LanguageProvider";
 
-class LoginScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lang: "vi"
-    };
-  }
+const onChangeLang = () => {};
 
-  render() {
-    const { lang } = this.state;
+function LoginScreen() {
+  const context = useContext(LanguageContext);
+  const { lang } = context.state;
+  console.log(context);
 
-    return (
-      <View>
-        <StatusBar
-          barStyle={"light-content"}
-          translucent={true}
-          backgroundColor={"transparent"}
-        />
-        <LoginScreenHeader lang={lang} />
-        <LoginScreenInputPhoneNumber
-          onChangeLang={this.onChangeLang}
-          lang={lang}
-        />
-        <GlobalChatPopUpIcon />
-      </View>
-    );
-  }
-
-  onChangeLang = () => {
-    const { lang } = this.state;
-    const newLang = lang === "en" ? "vi" : "en";
-
-    console.log("new lange:", newLang);
-    this.setState({
-      lang: newLang
-    });
-  };
+  return (
+    <View>
+      <StatusBar
+        barStyle={"light-content"}
+        translucent={true}
+        backgroundColor={"transparent"}
+      />
+      <LoginScreenHeader lang={lang} />
+      <LoginScreenInputPhoneNumber onChangeLang={onChangeLang} lang={lang} />
+      <GlobalChatPopUpIcon />
+    </View>
+  );
 }
 
 export default LoginScreen;
