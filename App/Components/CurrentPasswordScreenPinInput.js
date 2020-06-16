0dea587243e434eb38209513b71aa6655a973260
Veiler <linhnv110196@gmail.com>
2020-06-16 17:49:51 +0700

    convert screen đã sống  nhưng không input đc gì

--- App/Components/CurrentPasswordScreenPinInput.js
+++ App/Components/CurrentPasswordScreenPinInput.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { View } from "react-native";
 import SmoothPinCodeInput from "react-native-smooth-pincode-input";
 import { navigationRef } from "../Navigation/RootNavigation";
@@ -9,49 +9,37 @@ function navigation(name) {
   navigationRef.current && navigationRef.current.navigate(name);
 }
 
-export default class PinInput extends React.Component {
-  state = {
-    password: ""
-  };
-
-  render() {
-    const { password } = this.state;
-
-    return (
-      <View style={styles.container}>
-        <View style={styles.section}>
-          <SmoothPinCodeInput
-            password
-            mask=<View
-              style={{
-                width: 20,
-                height: 20,
-                borderRadius: 34,
-                backgroundColor: "rgb(114, 13, 93)"
-              }}
-            />
-            cellStyleFocused={{
-              borderColor: colors.blueGrey
-            }}
-            cellSize={28}
-            codeLength={6}
-            value={password}
-            onTextChange={this.onTextChange}
-            cellStyle={styles.cellStyle}
-            autoFocus={true}
-          />
-        </View>
-      </View>
-    );
+export default function PinInput() {
+  const [password, SetPassword] = useState("");
+  function onTextChange(text) {
+    const navigate = text.length >= 6 ? navigation("PinCode2") : "" ;
+    // const navigate = text.length >= 6 ?
   }
 
-  onTextChange = text => {
-    const navigate = text.length >= 6 ? navigation("PinCode2") : null;
-    this.setState(
-      {
-        password: text
-      },
-      navigate
-    );
-  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.section}>
+        <SmoothPinCodeInput
+          password
+          mask=<View
+            style={{
+              width: 20,
+              height: 20,
+              borderRadius: 34,
+              backgroundColor: "rgb(114, 13, 93)"
+            }}
+          />
+          cellStyleFocused={{
+            borderColor: colors.blueGrey
+          }}
+          cellSize={28}
+          codeLength={6}
+          value={password}
+          onTextChange={onTextChange}
+          cellStyle={styles.cellStyle}
+          autoFocus={true}
+        />
+      </View>
+    </View>
+  );
 }
