a068fc2a46dab180eb6e5e8596b9dd7e54ac43c0
quandepzai <43404519+quandepzai@users.noreply.github.com>
2020-06-16 10:00:27 +0700

    image

--- App/Images/images.js
+++ App/Images/images.js
@@ -1,4 +1,17 @@
 const images = {
-  backGround: require('./imageBG/BG.png')
+  backGround: require('./imageBG/BG.png'),
+  iconHome: require('./iconBottom/home.png'),
+  iconVoucher: require('./iconBottom/voucher.png'),
+  iconWallet: require('./iconBottom/wallet.png'),
+  iconUser: require('./iconBottom/account.png'),
+  banner: require('./banner/banner.png'),
+  icon1: require("./category/ico1.png"),
+  icon2: require("./category/ico2.png"),
+  icon3: require("./category/ico3.png"),
+  icon4: require("./category/ico4.png"),
+  layer1: require('./banner/Layer 1.png'),
+  pro1: require('./product/iphone.png'),
+  pro2: require('./product/xbox.png'),
+  pro3: require('./product/xbox.png')
 };
 export default images;
