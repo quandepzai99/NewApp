c65e869bcde9827b5115761354f9638e473ab3d2
Veiler <linhnv110196@gmail.com>
2020-06-16 09:51:02 +0700

    Add style News & Products

--- App/Components/HomeScreenNews.js
+++ App/Components/HomeScreenNews.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import {
   View,
-  StyleSheet,
   Text,
   FlatList,
   TouchableOpacity,
@@ -14,6 +13,7 @@ import Page1 from "./HomeScreenDetailPage1";
 import Page2 from "./HomeScreenDetailPage2";
 import Page3 from "./HomeScreenDetailPage3";
 import { navigationRef } from "../Navigation/RootNavigation";
+import styles from "./styles/HomeScreenNewsStyle";
 
 function navigate(name, id) {
   navigationRef.current && navigationRef.current.navigate(name, id);
@@ -117,59 +117,3 @@ export default class HomeScreenNews extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    top: 10
-  },
-  title: {
-    marginLeft: 16,
-    fontSize: 24,
-    lineHeight: 26,
-    letterSpacing: 0,
-    color: colors.velvet
-  },
-  btnBanner: {
-    top: 10,
-    borderRadius: 12,
-    marginHorizontal: 16,
-    alignItems: "center",
-    width: 368,
-    height: 260,
-    backgroundColor: "#ffffff",
-    shadowColor: "rgba(22, 60, 132, 0.16)",
-    shadowOffset: {
-      width: 0,
-      height: 10
-    },
-    shadowRadius: 10,
-    shadowOpacity: 1,
-    elevation: 5
-  },
-  desc: {
-    paddingVertical: 10,
-    fontSize: 15,
-    paddingRight: 30,
-    padding: 10,
-    fontWeight: "500",
-    fontStyle: "normal",
-    lineHeight: 20,
-    letterSpacing: 0,
-    color: colors.greyishBrown
-  },
-  name: {
-    fontSize: 12,
-    fontWeight: "500",
-    fontStyle: "normal",
-    textAlign: "left",
-    color: colors.blueGrey
-  },
-  divider: {
-    paddingTop: 5,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    borderColor: colors.paleGreyFour,
-    borderTopWidth: 1,
-    left: 10,
-    marginRight: 20
-  }
-});
--- App/Components/HomeScreenProduct.js
+++ App/Components/HomeScreenProduct.js
@@ -1,15 +1,14 @@
 import React from "react";
 import {
   View,
-  StyleSheet,
   Text,
   Image,
   FlatList,
   TouchableOpacity,
-  Dimensions
+
 } from "react-native";
 import images from "../Images/images";
-import colors from "../Themes/colors";
+import styles from "./styles/HomeScreenProductStyle"
 
 const proData = [
   {
@@ -71,76 +70,4 @@ export default function HomeScreenProduct() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "normal",
-    fontStyle: "normal",
-    lineHeight: 30,
-    letterSpacing: 0,
-    textAlign: "left",
-    color: colors.velvet,
-    justifyContent: "center"
-  },
-  itemProduct: {
-    justifyContent: "space-between",
-    top: 10,
-    width: (180 / 414) * Dimensions.get("screen").width,
-    height: (264 / 736) * Dimensions.get("screen").height,
-    paddingTop: 17,
-    marginLeft: 16,
-    borderRadius: 12,
-    backgroundColor: "white",
-    shadowColor: "rgba(22, 60, 132, 0.16)",
-    shadowOffset: {
-      width: 0,
-      height: 3
-    },
-    shadowRadius: 10,
-    shadowOpacity: 1,
-    elevation: 5
-  },
-  desc: {
-    fontSize: (15 / 736) * Dimensions.get("screen").height,
-    lineHeight: (18 / 736) * Dimensions.get("screen").height,
-    letterSpacing: 0,
-    paddingTop: 20,
-    paddingLeft: 10,
-    textAlign: "left",
-    justifyContent: "center",
-    color: colors.greyishBrown
-  },
-  price: {
-    alignItems: "center",
-    fontSize: 17,
-    fontWeight: "500",
-    fontStyle: "normal",
-    lineHeight: 17,
-    paddingLeft: 12,
-    paddingTop: 15,
-    color: colors.velvet
-  },
-  xemTtC: {
-    fontSize: 15,
-    fontWeight: "normal",
-    fontStyle: "normal",
-    lineHeight: 30,
-    color: colors.deepSkyBlue,
-    textAlign: "center"
-  },
-  box: {
-    borderBottomLeftRadius: 12,
-    borderBottomRightRadius: 12,
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  plus: {
-    padding: 13,
-    backgroundColor: colors.tangerine,
-    borderTopLeftRadius: 12,
-    borderBottomRightRadius: 12
-  }
-});
+
