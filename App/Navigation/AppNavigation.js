89ee69acf00355b16eb7c4edd13933d6b88c17fe
murkrow92 <ducdt199@gmail.com>
2020-06-16 15:44:37 +0700

    add app navigation

---
+++ App/Navigation/AppNavigation.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "../Containers/LoginScreen";
+import CurrentPassWordScreen from "../Containers/CurrentPassWordScreen";
+
+import { navigationRef } from "../Navigation/RootNavigation";
+const Stack = createStackNavigator();
+
+function AppNavigation() {
+  return (
+    <NavigationContainer ref={navigationRef}>
+      <Stack.Navigator headerMode="none">
+        <Stack.Screen name={"Telephone"} component={LoginScreen} />
+        {/*<Stack.Screen name={"PinCode"} component={PasswordScreen} />*/}
+        {/*<Stack.Screen name={"PinCode1"} component={CurrentPassWordScreen} />*/}
+        {/*<Stack.Screen name={"PinCode2"} component={ChangePassword} />*/}
+        {/*<Stack.Screen name={"Chats"} component={Inbox} />*/}
+        {/*<Stack.Screen name={"Home"} component={Bottom} />*/}
+        {/*<Stack.Screen name={"Page1"} component={Page1} />*/}
+        {/*<Stack.Screen name={"Page2"} component={Page2} />*/}
+        {/*<Stack.Screen name={"Page3"} component={Page3} />*/}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default AppNavigation;
