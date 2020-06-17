daee16594869086fd58b61b77fee745d37738a42
EagleVee <35548796+EagleVee@users.noreply.github.com>
2020-06-17 10:41:30 +0700

    add comment

--- App/ReduxHooks/LanguageReducer.js
+++ App/ReduxHooks/LanguageReducer.js
@@ -3,7 +3,12 @@ import { LanguageActions } from "./LanguageActions";
 export const initialState = {
   language: "vi"
 };
-
+// Reducer
+// Khi 1 action được dispatch, sẽ được chuyển đến reducer
+// VD: action SET_LANGUAGE được dispatch với data: { type: "SET_LANGUAGE", payload: "en" }
+// thì reducer sẽ tìm và thực hiện việc thay đổi state ứng với action đó, trong trường hợp
+// ở đây thì thay đổi language thành "en"
+// Reducer nên sử dụng syntax switch(action.type)
 export const LanguageReducer = (state, action) => {
   if (action.type === LanguageActions.setLanguage) {
     return { ...state, language: action.payload };
