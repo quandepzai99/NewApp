7f7494525f4d26904a6c9689e819e1a2a7ed0d4b
Veiler <linhnv110196@gmail.com>
2020-06-18 10:02:45 +0700

    ConVert man` CurrentPassword sang hook, fix styles

--- App/Components/ChangePasswordScreenPinInput.js
+++ App/Components/ChangePasswordScreenPinInput.js
@@ -2,7 +2,6 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 import SmoothPinCodeInput from "react-native-smooth-pincode-input";
 import colors from "../Themes/Colors";
-
 export default class PinInput extends React.Component {
   state = {
     code: "",
--- App/Components/styles/CurrentPasswordScreenPinInputStyle.js
+++ App/Components/styles/CurrentPasswordScreenPinInputStyle.js
@@ -1,31 +1,39 @@
-import {StyleSheet} from 'react-native';
-import colors from '../../Themes/Colors';
+import { StyleSheet } from "react-native";
+import colors from "../../Themes/Colors";
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "white",
-        alignItems: "center",
-        justifyContent: "center",
-        borderRadius: 12,
-        borderColor: "rgb(230,236,240)",
-        borderWidth: 1
-    },
-    section: {
-        alignItems: "center",
-        margin: 16
-    },
-    title: {
-        fontSize: 16,
-        fontWeight: "bold",
-        marginBottom: 8
-    },
-    cellStyle: {
-        borderRadius: 24,
-        borderColor: colors.paleGreyFour,
-        borderWidth: 1,
-        marginLeft: 12
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 12,
+    borderColor: "rgb(230,236,240)",
+    borderWidth: 1
+  },
+  section: {
+    alignItems: "center",
+    margin: 4
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  cellStyle: {
+      borderWidth: 1,
+      borderRadius: 24,
+      width: 28,
+      height: 28,
+      borderColor: colors.paleGreyFour,
+      backgroundColor: "#fff",
+      marginLeft: 16
+  },
+  maskStyle: {
+    width: 17,
+    height: 17,
+    borderRadius: 25,
+    backgroundColor: colors.velvet
+  }
 });
 
 export default styles;
