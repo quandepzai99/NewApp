337f4b2e9efda18649c1d5e063772612ce1515e7
quandepzai <43404519+quandepzai@users.noreply.github.com>
2020-06-22 21:43:52 +0700

    action, reducer 2

--- App/Components/PasswordScreenInputPassword.js
+++ App/Components/PasswordScreenInputPassword.js
@@ -1,17 +1,21 @@
 import React, { useContext, useState } from "react";
-import { View, Text, TouchableOpacity, Image } from "react-native";
+import {View, Text, TouchableOpacity, Image, Alert} from 'react-native';
 import colors from "../Themes/Colors";
 import images from "../Images/images";
 import SmoothPinCodeInput from "react-native-smooth-pincode-input";
 import { navigate } from "../Navigation/RootNavigation";
 import styles from "./styles/PasswordScreenInputPasswordStyle";
 import { LanguageContext } from "../Providers/LanguageProvider";
+import {PassContext, PassProvider} from '../Providers/PassProvider';
 
 
 export default function PasswordScreenInputPassword() {
   const languageContext = useContext(LanguageContext);
   const { content } = languageContext.state;
   const [code, setCode] = useState("");
+  const passContext = useContext(PassContext);
+  const {isPasswordExist} = passContext;
+  const TextCode = {isPasswordExist, code}
 
   return (
     <View style={styles.container}>
@@ -36,7 +40,7 @@ export default function PasswordScreenInputPassword() {
         password={true}
         autoFocus={true}
         codeLength={6}
-        onFulfill={() => navigate("Home")}
+        onFulfill={() => TextCode}
       />
       <View style={styles.box}>
         <TouchableOpacity
@@ -59,3 +63,22 @@ export default function PasswordScreenInputPassword() {
     </View>
   );
 }
+
+const getOnPress = (isActive, isPhoneNumberExist, phone) => {
+  return isActive
+    ? () => {
+      console.log("IS EXIST:", phone);
+      isPhoneNumberExist(phone, onSuccess, onFailed);
+    }
+    : () => {};
+};
+
+const onSuccess = isExist => {
+  if (isExist) {
+    navigate("Home");
+  } else {
+    Alert.alert("So dien thoai nay chua ton tai");
+  }
+};
+
+const onFailed = () => {};
--- App/ReduxHooks/PassReducer.js
+++ App/ReduxHooks/PassReducer.js
@@ -4,3 +4,13 @@ export const initialState = {
   password: '',
   isExist: false
 }
+
+export const PassReducer = (state, action) => {
+  if (action.type === PassAction.isPasswordNumberExist) {
+    const {password} = action.payload;
+    return {...state, password: content}
+  } else {
+    return state;
+  }
+}
+export default initialState;
