012cac1501bacb07f024c3436265e2f6bada6be0
Veiler <linhnv110196@gmail.com>
2020-06-24 17:48:29 +0700

    add checkToken

--- App/Containers/LoginScreen.js
+++ App/Containers/LoginScreen.js
@@ -3,15 +3,26 @@ import { View, StatusBar, AppState } from "react-native";
 import LoginScreenInputPhoneNumber from "../Components/LoginScreenInputPhoneNumber";
 import LoginScreenHeader from "../Components/LoginScreenHeader";
 import GlobalChatPopUpIcon from "../Components/GlobalChatPopUpIcon";
+import API from "../Lib/API";
 
 function LoginScreen() {
   const handleAppStateChange = nextAppState => {
     if (nextAppState === "active") {
-      console.log("STATEEEE", nextAppState);
+      checkToken();
     }
   };
 
   AppState.addEventListener("change", handleAppStateChange);
+
+  async function checkToken() {
+    const response = await API.validateToken(token);
+    if (response.status) {
+      const { data } = response;
+      const { is_alive } = data;
+      console.log("status", response.status);
+      console.log("DATATATATA", data);
+    }
+  }
 
   return (
     <View>
