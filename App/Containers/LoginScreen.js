a73c9ee523633468ab0d05828442b9a631eb51e7
quandepzai <43404519+quandepzai@users.noreply.github.com>
2020-06-24 17:48:30 +0700

    state active

--- App/Containers/LoginScreen.js
+++ App/Containers/LoginScreen.js
@@ -3,14 +3,22 @@ import { View, StatusBar, AppState } from "react-native";
 import LoginScreenInputPhoneNumber from "../Components/LoginScreenInputPhoneNumber";
 import LoginScreenHeader from "../Components/LoginScreenHeader";
 import GlobalChatPopUpIcon from "../Components/GlobalChatPopUpIcon";
+import {LocalStorage} from '../Lib/LocalStorage';
+import {AuthActions} from '../ReduxHooks/AuthActions';
 
 
 function LoginScreen() {
   const handleAppStateChange = nextAppState => {
     if (nextAppState === "active") {
       console.log("STATEEEE", nextAppState);
+      // const user = {
+      //   token: data
+      // };
+      // LocalStorage.set('user', user);
+      // nextAppState.dispatch(AuthActions(user))
     }
   };
+  console.log('asasa', handleAppStateChange)
 
   AppState.addEventListener("change", handleAppStateChange);
 
