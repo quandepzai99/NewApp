b22f47b5b1a546bcce314f238cd5b98602bc5faf
Veiler <linhnv110196@gmail.com>
2020-06-26 15:40:37 +0700

    Sang CHANGE PASSWORD

--- App/ReduxHooks/AuthActions.js
+++ App/ReduxHooks/AuthActions.js
@@ -4,5 +4,8 @@ export const AuthActions = {
   isPhoneNumberExist: "IS_PHONE_NUMBER_EXIST",
   isPasswordCorrect: "IS_PASSWORD_CORRECT",
   validateToken: "VALIDATE_TOKEN",
-  logout: "LOG_OUT"
+  logout: "LOG_OUT",
+  isCurrentPasswordCorrect: "CHECK_CURRENT_PASSWORD",
+  changePassword: "CHANGE_PASSWORD"
 };
+
