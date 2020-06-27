8231c0422d38c13d3ad47203f286725c4bd6063b
Veiler <linhnv110196@gmail.com>
2020-06-27 09:17:52 +0700

    fix header Change Password

---
+++ App/Components/styles/ChangePasswordHeaderStyles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    width: 414,
+    height: 200,
+    justifyContent: "center"
+  },
+  headerText: {
+    fontSize: 32,
+    fontWeight: "bold",
+    fontStyle: "normal",
+    lineHeight: 40,
+    letterSpacing: 0,
+    textAlign: "center",
+    color: "#ffffff",
+    justifyContent: "center"
+  }
+});
+export default styles;
